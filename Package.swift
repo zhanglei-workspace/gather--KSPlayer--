c4875347833218ca7b26ceb107c9939d7613e732
Lyons <zhangleiworkspace@gmail.com>
2025-06-20 10:25:08 +0800

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -61,6 +61,6 @@ if FileManager.default.fileExists(atPath: ffmpegKitPath + "/Package.swift") {
     ]
 } else {
     package.dependencies += [
-        .package(url: "https://github.com/zhanglei-workspace/FFmpegKit.git", from: "6.1.3.1"),
+        .package(url: "https://github.com/zhanglei-workspace/FFmpegKit.git", from: "6.1.3-beta.1"),
     ]
 }
